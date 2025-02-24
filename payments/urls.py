106d21b6cdd00a164c946203757583535c30b409
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2025-02-24 19:02:40 +0300

    Add Spripe payment

--- payments/urls.py
+++ payments/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from payments.views import ItemView
+from payments.views import ItemView, buy_item
 
 app_name = 'payments'
 
 urlpatterns = [
     path('item/<int:item_id>', ItemView.as_view()),
+    path('buy/<int:item_id>', buy_item),
 ]
--- payments/views.py
+++ payments/views.py
@@ -2,6 +2,9 @@ from lib2to3.fixes.fix_input import context
 
 from django.conf import settings
 import stripe
+from django.http import JsonResponse
+from django.urls import reverse
+from django.views import View
 from django.views.generic import TemplateView
 
 from common.mixins.mixins import TitleMixin
@@ -21,6 +24,35 @@ class ItemView(TitleMixin, TemplateView):
             item = Item.objects.get(id=item_id)
 
             context["item"] = item
+            context["stripe_public_key"] = settings.STRIPE_PUBLIC_KEY
             return context
         except BaseException as e:
             print(e)
+            return JsonResponse({}, status=400)
+
+def buy_item(request, item_id):
+        try:
+            item = Item.objects.get(id=item_id)
+            checkout_session = stripe.checkout.Session.create(
+                payment_method_types=['card'],
+                line_items=[{
+                    'price_data': {
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': item.name,
+                        },
+                        'unit_amount': int(item.price * 100),
+                    },
+                    'quantity': 1,
+                }],
+                mode='payment',
+                success_url='http://localhost:8000/success/',
+                cancel_url='http://localhost:8000/cancel/',
+            )
+            return JsonResponse({
+                'id': checkout_session.id,
+                'url': checkout_session.url,
+            })
+        except BaseException as e:
+            print(e)
+            return JsonResponse({}, status=400)
