04a3db6a9e8e0678361491e91c20145796a9ce9d
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2025-02-24 22:07:34 +0300

    Fix JS

--- payments/urls.py
+++ payments/urls.py
@@ -5,7 +5,7 @@ from payments.views import ItemView, buy_item, buy_order
 app_name = 'payments'
 
 urlpatterns = [
-    path('item/<int:item_id>', ItemView.as_view()),
-    path('buy/<int:item_id>', buy_item),
-    path('buy_order/<int:order_id>', buy_order),
+    path('item/<int:item_id>', ItemView.as_view(), name="item"),
+    path('buy/<int:item_id>', buy_item, name="buy"),
+    path('buy_order/<int:order_id>', buy_order, name="buy_order"),
 ]
