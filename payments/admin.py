96541df249625f4c5355e109de4c928f255359ed
MarlerZ1 <91724246+MarlerZ1@users.noreply.github.com>
2025-02-24 17:33:44 +0300

    Register models in admin panel

--- payments/admin.py
+++ payments/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments.models import Item, User
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'description']
+
+
+@admin.register(User)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', "email", "is_staff"]
